930976c00d20fb02af1af054cc619bc5f32b2c6f
Martin Schmal <martinschmal@gmail.com>
2020-01-22 18:00:34 +0100

    refactoring

--- main.js
+++ main.js
@@ -6,12 +6,6 @@ const PLAYERHEIGHT = 50;
 
 const OBSTACLEHEIGHT = 50;
 let direction = -1;
-let spawnFrame1 = 0;
-let spawnFrame2 = 0;
-let spawnFrame3 = 0;
-let spawnFrame4 = 0;
-let spawnFrame5 = 0;
-
 const STEP = 50; // STEP should be euqal to PLAYERWIDTH/HEIGHT
 
 /// ----- Styling constants
@@ -40,8 +34,8 @@ class Game {
 		this.background = new Background();
 		this.obstacle = new Obstacle();
 		this.noObstacle = new NoObstacle();
-		this.player = new Player();
-		this.player2 = new Player();
+		this.player = new Player(lives1);
+		this.player2 = new Player(lives2);
 	}
 
 	setup() {
@@ -49,194 +43,129 @@ class Game {
 		this.player.setupPlayer1();
 		this.player2.setupPlayer2();
 	}
+	checkCollision(player, obst) {
+		if (
+			player.playerPosX + player.width / 2 > obst.x &&
+			player.playerPosX + player.width / 2 < obst.x + obst.width + player.width / 2
+		) {
+			if (
+				player.playerPosY + player.height / 2 > obst.y &&
+				player.playerPosY + player.height / 2 < obst.y + obst.height + player.height / 2
+			) {
+				player.dying();
+			}
+		}
+	}
+
+	checkTravel(player, nOobst) {
+		if (
+			player.playerPosX + player.width / 2 > nOobst.x &&
+			player.playerPosX + player.width / 2 < nOobst.x + nOobst.width + player.width / 2
+		) {
+			if (
+				player.playerPosY + player.height / 2 > nOobst.y &&
+				player.playerPosY + player.height / 2 < nOobst.y + nOobst.height + player.height / 2
+			) {
+				//player.travel();
+				player.playerPosX = nOobst.x + nOobst.width / 3;
+				player.playerPosY = nOobst.y;
+				player.draw(); // Player 1 is drawn on top of noObstacle
+
+				//player.livesHTML.innerHTML--;
+			}
+		}
+	}
 
 	draw() {
 		this.background.draw();
 		this.player.draw();
 		this.player2.draw();
 
-		/// Every 100 frames a new Object
+		/// Create new Obstacles
 		if (frameCount % 50 === 0) {
 			this.obstacles.push(new Obstacle());
 		}
 
-		if (frameCount % 120 === 0) {
-			let randomnumber = Math.floor(Math.random() * 5) + 1; // obstacles not in first and last row
-			// let width = Math.floor(Math.random() * 3) * 50 + 50; // obstacles not in first and last row
-			let x;
-			let y;
+		if (frameCount % 240 === 0) {
+			this.x = 800;
+			this.y = 300;
+			this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
 
-			switch (randomnumber) {
-				case 1:
-					if (frameCount > spawnFrame1 + 200) {
-						x = 800;
-						y = 300;
-						this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
-						spawnFrame1 = frameCount;
-					}
+			this.x = -100;
+			this.y = 350;
+			this.noObstacles.push(new NoObstacle(1, this.x, this.y));
 
-					break;
+			this.x = 800;
+			this.y = 400;
+			this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
 
-				case 2:
-					if (frameCount > spawnFrame2 + 200) {
-						this.x = -100;
-						this.y = 350;
-						this.noObstacles.push(new NoObstacle(1, this.x, this.y));
-						spawnFrame2 = frameCount;
-					}
+			this.x = -100;
+			this.y = 450;
+			this.noObstacles.push(new NoObstacle(1, this.x, this.y));
 
-					break;
-
-				case 3:
-					if (frameCount > spawnFrame3 + 200) {
-						this.x = 800;
-						this.y = 400;
-						this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
-						spawnFrame3 = frameCount;
-					}
-
-				
-					break;
-
-				case 4:
-					if (frameCount > spawnFrame4 + 200) {
-						this.x = -100;
-						this.y = 450;
-						this.noObstacles.push(new NoObstacle(1, this.x, this.y));
-						spawnFrame4 = frameCount;
-					}
-
-					break;
-
-				case 5:
-					if (frameCount > spawnFrame5 + 20) {
-						this.x = 800;
-						this.y = 500;
-						this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
-						spawnFrame5 = frameCount;
-					}
-
-					break;
-			}
+			this.x = 800;
+			this.y = 500;
+			this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
 		}
 		/// delete if more than 10 obstacles
-		if (this.obstacles.length > 10) {
+		if (this.obstacles.length > 20) {
 			this.obstacles.shift();
 		}
 
 		/// delte if more than 10 obstacles
-		if (this.noObstacles.length > 100) {
+		if (this.noObstacles.length > 50) {
 			this.noObstacles.shift();
 		}
 
-		/// ---------------------------Obstacle collision - Player 1 reset to start
-		this.obstacles.forEach(function(obstacle) {
+		/// Obstacle collision - Players reset to start
+		this.obstacles.forEach((obstacle) => {
 			obstacle.draw();
-			if (
-				game.player.playerPosX + game.player.width / 2 / 2 > obstacle.x &&
-				game.player.playerPosX + game.player.width / 2 < obstacle.x + obstacle.width + game.player.width / 2
-			) {
-				if (
-					game.player.playerPosY + game.player.height / 2 > obstacle.y &&
-					game.player.playerPosY + game.player.height / 2 <
-						obstacle.y + obstacle.height + game.player.height / 2
-				) {
-					// on collission kill player1
-					game.player.setupPlayer1();
-					lives1.innerHTML--;
-				}
-			}
-
-			/// ---------------------------Obstacle collision - Player 2 reset to start
-			if (
-				game.player2.playerPosX + game.player2.width > obstacle.x &&
-				game.player2.playerPosX + game.player2.width < obstacle.x + obstacle.width + game.player2.width
-			) {
-				if (
-					game.player2.playerPosY + game.player2.height > obstacle.y &&
-					game.player2.playerPosY + game.player2.height < obstacle.y + obstacle.height + game.player2.height
-				) {
-					// on collission kill player2
-					game.player2.setupPlayer2();
-					lives2.innerHTML--;
-				}
-			}
+			this.checkCollision(this.player, obstacle);
+			this.checkCollision(this.player2, obstacle);
 		});
 
-		/// ---------------------------No Obstacle collision - Player 1 travels with object
-		this.noObstacles.forEach(function(noObstacle) {
+		/// No-Obstacle collision - Players travelling with Object
+		this.noObstacles.forEach((noObstacle) => {
 			noObstacle.draw();
-			if (
-				game.player.playerPosX + game.player.width > noObstacle.x &&
-				game.player.playerPosX + game.player.width < noObstacle.x + noObstacle.width + game.player.width
-			) {
-				if (
-					game.player.playerPosY + game.player.height > noObstacle.y &&
-					game.player.playerPosY + game.player.height < noObstacle.y + noObstacle.height + game.player.height
-				) {
-					game.player.playerPosX = noObstacle.x + game.noObstacle.width / 3;
-					game.player.playerPosY = noObstacle.y;
-					game.player.draw(); // Player 1 is drawn on top of noObstacle
-				}
-			}
-
-			/// ---------------------------No Obstacle collision - Player 2 travels with object
-			if (
-				game.player2.playerPosX + game.player2.width > noObstacle.x &&
-				game.player2.playerPosX + game.player2.width < noObstacle.x + noObstacle.width + game.player2.width
-			) {
-				if (
-					game.player2.playerPosY + game.player2.height > noObstacle.y &&
-					game.player2.playerPosY + game.player2.height <
-						noObstacle.y + noObstacle.height + game.player2.height
-				) {
-					game.player2.playerPosX = noObstacle.x + game.noObstacle.width / 3;
-					game.player2.playerPosY = noObstacle.y;
-					game.player2.draw(); // Player 2 is drawn on top of noObstacle
-				}
-			}
-			//// --------------------------Other conditions for both Players
-
-			/// Player 1 reaches top top screen
-			if (game.player.playerPosY <= 0) {
-				score1.innerHTML++;
-				console.log("goal1");
-				game.player.setupPlayer1();
-			}
-
-			/// Player 2 reaches top top screen
-			if (game.player2.playerPosY <= 0) {
-				score2.innerHTML++;
-				console.log("goal2");
-				game.player2.setupPlayer2();
-			}
-
-			/// Player 1 is killd by travelling out of screen
-			if (game.player.playerPosX > WIDTH + 50 || game.player.playerPosX < 50) {
-				lives1.innerHTML--;
-				game.player.setupPlayer1();
-				console.log("kill 1");
-			}
-
-			/// Player 2 is killd by travelling out of screen
-			if (game.player2.playerPosX > WIDTH || game.player.playerPosX < 50) {
-				lives2.innerHTML--;
-				game.player2.setupPlayer2();
-				console.log("kill 2");
-			}
+			this.checkTravel(this.player, noObstacle);
+			this.checkTravel(this.player2, noObstacle);
 		});
+
+		/// Player 1 reaches top top screen
+		if (game.player.playerPosY <= 0) {
+			score1.innerHTML += 10;
+			game.player.setupPlayer1();
+		}
+
+		/// Player 2 reaches top top screen
+		if (game.player2.playerPosY <= 0) {
+			score2.innerHTML += 10;
+			game.player2.setupPlayer2();
+		}
+
+		/// Player 1 is killd by travelling out of screen
+		if (game.player.playerPosX > WIDTH + 50 || game.player.playerPosX < -50) {
+			lives1.innerHTML--;
+			game.player.setupPlayer1();
+		}
+
+		/// Player 2 is killd by travelling out of screen
+		if (game.player2.playerPosX > WIDTH + 50 || game.player2.playerPosX < -50) {
+			lives2.innerHTML--;
+			game.player2.setupPlayer2();
+		}
 	}
 }
 
 class Player {
-	constructor(playerNr, playerScore, playerLives) {
-		this.playerNr = playerNr;
-		this.playerScore = playerScore;
+	constructor(livesHTML) {
 		this.player1Image = loadImage("player-forward.png");
 		this.playerDead = loadImage("dead1.png");
+		this.state = true;
 		this.width = PLAYERWIDTH;
 		this.height = PLAYERHEIGHT;
 		this.playerLives = 0;
+		this.livesHTML = livesHTML;
 	}
 
 	setupPlayer1() {
@@ -257,20 +186,33 @@ class Player {
 	}
 
 	draw() {
-		image(this.player1Image, this.playerPosX, this.playerPosY, this.width, this.height);
+		
+		if (!this.state) {
+			image(game.player.playerDead, game.player.playerPosX, game.player.playerPosY, 50, 50);
+			if (frameCount % 120 === 0) {
+				this.state = true;
+				game.player.livesHTML.innerHTML--;
+				
+			}
+		}else {
+
+		image(this.player1Image, this.playerPosX, this.playerPosY, this.width, this.height);}
+
 	}
 
 	move(x, y) {
 		this.playerPosX += x;
 		this.playerPosY += y;
 	}
+	dying() {
+		this.state = false;
+	}
 }
 
 class Obstacle {
 	///----- Players are killed by this
 	constructor() {
-		//this.obstacleImage = loadImage("taxi.png");
-
+	
 		this.obstacleImage = [
 			{
 				src: loadImage("taxi.png")
@@ -288,7 +230,7 @@ class Obstacle {
 
 		this.width = 100;
 		this.height = 50;
-		this.x = -100;
+		this.x = Math.floor(Math.random() * -50) - 50;
 		this.y = Math.floor(Math.random() * 5) * 50 + 50; // obstacles not in first and last row
 		this.i = Math.floor(Math.random() * 4); // randomize the 4 images
 		this.counter = 0;
@@ -297,9 +239,7 @@ class Obstacle {
 	draw() {
 		let i = 1;
 		this.x += 3; // speed of obstacales
-
 		image(this.obstacleImage[this.i].src, this.x, this.y, this.width, this.height);
-		//console.log(game.obstacle.x);
 	}
 }
 
@@ -309,7 +249,7 @@ class NoObstacle {
 		this.obstacleImage = loadImage("wood.png");
 		this.width = Math.floor(Math.random() * 3) * 50 + 100;
 		this.height = 50;
-		this.x = x;
+		this.x = x - Math.floor(Math.random() * 50);
 		this.y = y;
 		this.counter = 0;
 		this.direction = direction;
@@ -345,6 +285,7 @@ function draw() {
 function keyPressed() {
 	/// --- Keys Player 1
 
+if (game.player.state = true){
 	if (keyCode === 37 && game.player.playerPosX > 0) {
 		game.player.player1Image = loadImage("player-right.png");
 		game.player.move(-STEP, 0);
@@ -364,6 +305,7 @@ function keyPressed() {
 		game.player.player1Image = loadImage("player-backward.png");
 		game.player.move(0, STEP);
 	}
+}
 
 	/// ---- Keys Player 2
 	if (keyCode === 65 && game.player2.playerPosX > 0) {
