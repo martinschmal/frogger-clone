e1c4c8b7dd24e5bf862b9367d6b8a647827213d4
Martin Schmal <martinschmal@gmail.com>
2020-01-23 16:02:12 +0100

    added points and end screen

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const HEIGHT = 650; // canvas height
 const WIDTH = 800; // canvas width
 const PLAYERWIDTH = 50;
 const PLAYERHEIGHT = 50;
-
 const OBSTACLEHEIGHT = 50;
-let direction = -1;
 const STEP = 50; // STEP should be euqal to PLAYERWIDTH/HEIGHT
+
+/// ----- Globals
+let  carSpawnSpeed = 80;
 
 /// ----- Styling constants
 const score1 = document.querySelector("#score1");
@@ -14,6 +15,7 @@ const score2 = document.querySelector("#score2");
 const lives1 = document.querySelector("#lives1");
 const lives2 = document.querySelector("#lives2");
 
+/// ----- 
 class Background {
 	constructor() {
 		this.backgroundImage = loadImage("bg2.png");
@@ -34,8 +36,8 @@ class Game {
 		this.background = new Background();
 		this.obstacle = new Obstacle();
 		this.noObstacle = new NoObstacle();
-		this.player = new Player(lives1);
-		this.player2 = new Player(lives2);
+		this.player = new Player(lives1, score1);
+		this.player2 = new Player(lives2, score2);
 	}
 
 	setup() {
@@ -69,10 +71,15 @@ class Game {
 				//player.travel();
 				player.playerPosX = nOobst.x + nOobst.width / 3;
 				player.playerPosY = nOobst.y;
-				player.draw(); // Player 1 is drawn on top of noObstacle
 
+				textSize(32);
+				text("Score: " + player.scoreHTML.innerHTML, player.playerPosX, player.playerPosY);
+				fill(0, 102, 153);
+
+				player.travelling();
+				player.draw(); // Player 1 is drawn on top of noObstacle
 			}
-		 }
+		}
 	}
 
 	draw() {
@@ -81,33 +88,33 @@ class Game {
 		this.player2.draw();
 
 		/// Create new Obstacles
-		if (frameCount % 50 === 0) {
+		if (frameCount % carSpawnSpeed === 0) {
 			this.obstacles.push(new Obstacle());
 		}
 
-		if (frameCount % 240 === 0) {
-			this.x = -100;
+		if (frameCount % 350 === 0 || frameCount === 0 ) {
+			this.x = -150;
 			this.y = 400;
 			this.noObstacles.push(new NoObstacle(1, this.x, this.y));
 
-			this.x = 800;
+			this.x = 950;
 			this.y = 450;
 			this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
 
-			this.x = -100;
+			this.x = -150;
 			this.y = 500;
 			this.noObstacles.push(new NoObstacle(1, this.x, this.y));
 
-			this.x = 800;
+			this.x = 950;
 			this.y = 550;
 			this.noObstacles.push(new NoObstacle(-1, this.x, this.y));
 		}
-		/// delete if more than 10 obstacles
+		/// delete if more than 10 obstacles in array
 		if (this.obstacles.length > 20) {
 			this.obstacles.shift();
 		}
 
-		/// delte if more than 10 obstacles
+		/// delete if more than 10 obstacles in arry
 		if (this.noObstacles.length > 50) {
 			this.noObstacles.shift();
 		}
@@ -125,71 +132,93 @@ class Game {
 			this.checkTravel(this.player, noObstacle);
 			this.checkTravel(this.player2, noObstacle);
 		});
-
-		/// Player 1 reaches top top screen
-		if (game.player.playerPosY <= 0) {
-			score1.innerHTML += 10;
+		/// reaches top top screen
+		/// Player 1
+		if (game.player.playerPosY < 0) {
+			game.player.win();
 			game.player.setupPlayer1();
 		}
 
-		/// Player 2 reaches top top screen
-		if (game.player2.playerPosY <= 0) {
-			score2.innerHTML += 10;
+		/// Player 2
+		if (game.player2.playerPosY < 0) {
+			game.player2.win();
 			game.player2.setupPlayer2();
 		}
-
-		/// Player 1 is killd by travelling out of screen
+		/// killd by travelling out of screen
+		/// Player 1
 		if (game.player.playerPosX > WIDTH + 50 || game.player.playerPosX < -50) {
-			lives1.innerHTML--;
+			game.player.dying();
 			game.player.setupPlayer1();
 		}
 
-		/// Player 2 is killd by travelling out of screen
+		/// Player 2
 		if (game.player2.playerPosX > WIDTH + 50 || game.player2.playerPosX < -50) {
-			lives2.innerHTML--;
+			game.player2.dying();
 			game.player2.setupPlayer2();
 		}
 	}
 }
 
 class Player {
-	constructor(livesHTML) {
+	constructor(livesHTML, scoreHTML) {
 		this.player1Image = loadImage("player-forward.png");
 		this.playerDead = loadImage("dead1.png");
 		this.state = true;
 		this.width = PLAYERWIDTH;
 		this.height = PLAYERHEIGHT;
-		this.playerLives = 0;
 		this.livesHTML = livesHTML;
+		this.scoreHTML = scoreHTML;
 	}
 
 	setupPlayer1() {
 		// initial position of player 1
 		this.playerPosX = WIDTH / 2 + STEP;
 		this.playerPosY = HEIGHT - STEP;
+		this.width = 50 * 0.9;
+		this.height = 50 * 0.9;
 	}
 
 	setupPlayer2() {
 		// initial position of player 1
 		this.playerPosX = WIDTH / 2 - STEP;
 		this.playerPosY = HEIGHT - STEP;
-	}
-
-	killPlayer() {
-		// initial position of player 1
-		this.playerLives--;
+		this.width = 50 * 1.1;
+		this.height = 50 * 1.1;
 	}
 
 	draw() {
 		if (!this.state) {
 			image(this.playerDead, this.playerPosX, this.playerPosY, 50, 50);
-			if (frameCount % 120 === 0) {
+
+			if (frameCount % 180 === 0 && this.livesHTML.innerHTML > 0) {
 				this.state = true;
 				this.livesHTML.innerHTML--;
-				//setupPlayer1();
+
+				this.playerPosX = WIDTH / 2 + STEP;
+				this.playerPosY = HEIGHT - STEP;
 			}
 		} else {
 			image(this.player1Image, this.playerPosX, this.playerPosY, this.width, this.height);
+		}
+		this.checkGameOver();
+	}
+
+	checkGameOver() {
+		if (game.player.state === false && game.player2.state === false) {
+			if (game.player.livesHTML.innerHTML == 0 && game.player2.livesHTML.innerHTML == 0) {
+				textSize(120);
+				text("Game Over", 50, 500);
+				fill(255, 255, 255);
+				textSize(48);
+				text("Small Frog has " + game.player.scoreHTML.innerHTML + " Points", 100, 150);
+				text("Big Frog has " + game.player2.scoreHTML.innerHTML + " Points", 100, 200);
+				fill(255, 255, 255);
+
+				if (parseInt(game.player.scoreHTML.innerHTML) > parseInt(game.player2.scoreHTML.innerHTML))
+					text("Small Frog wins!", 100, 300);
+				else text("Big Frog wins!", 100, 300);
+
+							}
 		}
 	}
 
@@ -201,6 +230,25 @@ class Player {
 	}
 	dying() {
 		this.state = false;
+		//this.livesHTML.innerHTML--;
+	}
+
+	travelling() {
+		this.state = true;
+		this.scoreHTML.innerHTML++;
+	}
+
+	win() {
+		this.state = true;
+		carSpawnSpeed = carSpawnSpeed -1;
+
+		textSize(48);
+		text("Score: " + this.scoreHTML.innerHTML, this.playerPosX, this.playerPosY);
+		fill(0, 102, 193);
+		console.log(this);
+		let a = parseInt(this.scoreHTML.innerHTML);
+		a = a + 500;
+		this.scoreHTML.innerHTML = a; // 1000;
 	}
 }
 
@@ -290,7 +338,7 @@ function keyPressed() {
 			game.player.move(STEP, 0);
 		}
 
-		if (keyCode === 38 && game.player.playerPosY > 0) {
+		if (keyCode === 38 && game.player.playerPosY >= 0) {
 			game.player.player1Image = loadImage("player-forward.png");
 			game.player.move(0, -STEP);
 		}
@@ -300,7 +348,7 @@ function keyPressed() {
 			game.player.move(0, STEP);
 		}
 	}
-	if (game.player.state === true) {
+	if (game.player2.state === true) {
 		/// ---- Keys Player 2
 		if (keyCode === 65 && game.player2.playerPosX > 0) {
 			game.player2.player1Image = loadImage("player-right.png");
@@ -312,7 +360,7 @@ function keyPressed() {
 			game.player2.move(STEP, 0);
 		}
 
-		if (keyCode === 87 && game.player2.playerPosY > 0) {
+		if (keyCode === 87 && game.player2.playerPosY >= 0) {
 			game.player2.player1Image = loadImage("player-forward.png");
 			game.player2.move(0, -STEP);
 		}
